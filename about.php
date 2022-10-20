71f77fafd651478b65c6faf02ed4ebc27d062602
MangOo22 <mangaa99999@gmail.com>
2022-10-20 12:26:53 +0200

    Basic-Banking-System

---
+++ about.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" sizes="180x180" href="statics/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="statics/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="statics/favicon-16x16.png">
+    <link rel="manifest" href="statics/site.webmanifest">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>About Me - Sparks Bank</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+
+<body style="background-color:#fff;">
+    <?php include 'navbar.php'; ?>
+    <center>
+        <div class="container" style="margin-top: 9%; padding:10px 80px 10px 80px; ">
+            <p style="color:white;">This website is created by</p>
+            <h1 style="color:black; text-shadow:2px 2px 2px gray;">Ahmed Nageh</h1>
+            <p style="color:black;"><b>Web Development and Designing Intern at The Sparks Foundation</p>
+
+        </div>
+    </center>
+    </b<ody>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,26 @@
+<!-- navbar -->
+<nav class="navbar navbar-expand-md navbar-light bg-light" style="background-color:#57b894 !important">
+    <a class="navbar-brand" href="index.php" style="font-size:30px; font-weight:bold;color : #f8f9fa;"> BANK</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #f8f9fa;"><b>Home</b></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : #f8f9fa;"><b>Create User</b></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #f8f9fa;"><b>Transfer Money</b></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #f8f9fa;"><b>Transaction
+                        History</b></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="about.php" style="color : #f8f9fa;"><b>About Me</b></a>
+            </li>
+    </div>
+</nav>
